a5e509f412c6847411fc160147faecc3f7d84b2d
Jacob <jakebuilds@gmail.com>
2026-08-02 17:03:30 -0400

    Update options.js

--- utils/overlays/scripts/options.js
+++ utils/overlays/scripts/options.js
@@ -2,17 +2,14 @@
 async function loadData(type) {
 	try {
 		const filePath = "https://tobographic.xyz/utils/overlays/data/"+type+".json";
-		console.log(filePath);
 		
 		const response = await fetch(filePath);
 		if (!response.ok) throw new Error(`Status: ${response.status}`);
 		
 		const jsonData = await response.json();
-		console.log(jsonData);
 		
 		// build options from data
 		const dropdown = document.getElementById(type);
-		console.log(dropdown);
 		
 		Object.values(jsonData).forEach(item => {
 			const option = document.createElement('option');
