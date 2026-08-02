d2a0fcb7476b1be502bb3e5ba3d67532862cfc2c
Jacob <jakebuilds@gmail.com>
2026-08-02 17:01:50 -0400

    refactor script to type agnostic

--- utils/overlays/scripts/options.js
+++ utils/overlays/scripts/options.js
@@ -1,28 +1,29 @@
 // fetch resolution data from appropriate json entries
-async function loadData() {
+async function loadData(type) {
 	try {
-		const types = ['resolution', 'console'];
+		const filePath = "https://tobographic.xyz/utils/overlays/data/"+type+".json";
+		console.log(filePath);
 		
-		const response = await fetch('https://tobographic.xyz/utils/overlays/data/resolution.json');
+		const response = await fetch(filePath);
 		if (!response.ok) throw new Error(`Status: ${response.status}`);
 		
 		const jsonData = await response.json();
 		console.log(jsonData);
 		
 		// build options from data
-		Array.from(types).forEach(child => {
-			const dropdown = document.getElementById(child);
-			
-			Object.values(jsonData).forEach(item => {
-				const option = document.createElement('option');
-				option.value = item.id;
-				option.text = item.name;
-				dropdown.appendChild(option);
-			});
+		const dropdown = document.getElementById(type);
+		console.log(dropdown);
+		
+		Object.values(jsonData).forEach(item => {
+			const option = document.createElement('option');
+			option.value = item.id;
+			option.text = item.name;
+			dropdown.appendChild(option);
 		});
 	} catch (error) {
 		console.error("Failed to read JSON:", error);
 	}
 }
 
-loadData();
+loadData("resolution");
+loadData("console");
